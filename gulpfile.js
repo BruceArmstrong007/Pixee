1bff54834f63ca98b22b6c5916e251f251868574
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-03-07 18:20:42 +0530

    Replicated from my SASS Repo

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss');
+
+
+
+function GenerateCSS() {
+    return src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
+        .pipe(dest('css'))
+}
+
+function watchSASS() {
+    watch(["pixee-ui/**/*.scss", 'sass/**/*.scss', '*.html'], GenerateCSS)
+}
+
+
+
+function build() {
+    return src('pixee-ui/**/*.scss')
+        .pipe(dest('dist'))
+}
+
+
+
+function buildCSS() {
+    return src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist'))
+}
+
+exports.build = series(buildCSS, build);
+exports.default = series(GenerateCSS, watchSASS);
