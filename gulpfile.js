aaa33704ff9bd49265738a775fc6644131639d0e
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-03-11 09:38:21 +0530

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss');
 
-
+// Generate 
 
 function GenerateCSS() {
     return src('sass/**/*.scss')
@@ -15,19 +15,22 @@ function watchSASS() {
     watch(["pixee-ui/**/*.scss", 'sass/**/*.scss', '*.html'], GenerateCSS)
 }
 
+exports.default = series(GenerateCSS, watchSASS);
 
+
+
+// Build
 
 function build() {
     return src('pixee-ui/**/*.scss')
         .pipe(dest('dist'))
 }
 
-
-
 function buildCSS() {
     return src('sass/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('dist'))
 }
 
 exports.build = series(buildCSS, build);
-exports.default = series(GenerateCSS, watchSASS);
